48a5f4505d4d582a6a154c17161c8a1c08ee9ee2
muzammilshaikh12 <110051885+muzammilshaikh12@users.noreply.github.com>
2022-11-04 14:35:39 +0530

    API in react

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const getMovieHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedData = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedData);
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={getMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
